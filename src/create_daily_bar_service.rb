22e6933e08d002b85940412be6eff473f8c25acd
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-03-10 13:44:28 +0900

    shogo作成

--- src/create_daily_bar_service.rb
+++ src/create_daily_bar_service.rb
@@ -1,6 +1,20 @@
+require_relative './daily_bar'
+
 class CreateDailyBarService
+
+  def initialize(prices)
+    @prices = prices
+  end
+
   # @return [DailyBar]
+
   def call
-    2
+    DailyBar.new(
+      code: @prices[1][0],
+      date: @prices[2][1],
+      open: @prices[1][2],
+      high: @prices[3].max,
+      low: @prices[3].min,
+      close: @prices.last[2])
   end
 end
---
+++ src/daily_bar.rb
@@ -0,0 +1,16 @@
+class DailyBar
+  attr_reader :code, :date, :open, :high, :low, :close
+
+  def initialize(code:, date:, open:, high:, low:, close:)
+    @code = code
+    @date = date
+    @open = open
+    @high = high
+    @low = low
+    @close = close
+  end
+
+  def white_candlestick?
+    false
+  end
+end
