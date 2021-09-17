a394b62889c0cb21ceafff9f761dcd29ea9f7ac0
mako0619 <stardust.shinkan@gmail.com>
2021-09-17 19:41:24 +0900

    選考ペアプロ第一回

--- spec/create_daily_bar_service_spec.rb
+++ spec/create_daily_bar_service_spec.rb
@@ -1,8 +1,9 @@
 require './spec_helper'
 require_relative '../src/create_daily_bar_service'
+require_relative '../src/daily_bar'
 
 describe CreateDailyBarService do
-  let(:prices) do 
+  let(:prices) do
     [
       ['銘柄コード', '時刻', '株価'],
       ['4436', '2021-3-10 10:00', 3585],
@@ -20,27 +21,27 @@ describe CreateDailyBarService do
     expect(daily_bar.code).to eq('4436')
   end
 
-  it '日付' do
-    expect(daily_bar.date).to eq(Date.new(2021,3,10))
-  end
+  # it '日付' do
+  #   expect(daily_bar.date).to eq(Date.new(2021,3,10))
+  # end
 
-  it '始値' do
-    expect(daily_bar.open).to eq(3605)
-  end
+  # it '始値' do
+  #   expect(daily_bar.open).to eq(3605)
+  # end
 
   it '高値' do
     expect(daily_bar.high).to eq(3655)
   end
 
-  it '安値' do
-    expect(daily_bar.low).to eq(3505)
-  end
+  # it '安値' do
+  #   expect(daily_bar.low).to eq(3505)
+  # end
 
-  it '終値' do
-    expect(daily_bar.close).to eq(3635)
-  end
+  # it '終値' do
+  #   expect(daily_bar.close).to eq(3635)
+  # end
 
-  it '陽線ならば真' do
-    expect(daily_bar.white_bar?).to be(true)
-  end
+  # it '陽線ならば真' do
+  #   expect(daily_bar.white_bar?).to be(true)
+  # end
 end
